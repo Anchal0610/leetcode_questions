61bf52091af8db47c78df453cc6c10e40445a081
Anchal Gupta <anchalgupta0602@gmail.com>
2026-08-24 00:45:18 +0530

    Time: 0 ms (100%), Space: 42.5 MB (88.26%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int rob(int[] nums) {
+        int n = nums.length;
+        int []dp = new int[n];
+        for(int i=0 ; i<n ; i++){
+            Arrays.fill(dp , -1);
+        }
+        return solve(n-1 , nums , dp);
+    }
+    public int solve(int index , int[] nums , int[]dp){
+        if(index == 0){
+            return nums[index];
+        }
+        if(index < 0){
+            return 0;
+        }
+        if(dp[index] != -1){
+            return dp[index];
+        }
+        int pick = nums[index] + solve(index-2 , nums , dp);
+        int notPick = 0 + solve(index-1 , nums ,dp);
+
+        return dp[index] = Math.max(pick , notPick);
+    }
+}
