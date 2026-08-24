7ab7d4506f0794146e8d33a10aa0ca0acb8525d0
Anchal Gupta <anchalgupta0602@gmail.com>
2026-08-25 00:10:06 +0530

    Time: 6 ms (10.05%), Space: 50.3 MB (38.76%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -6,22 +6,22 @@ class Solution {
        for(int i=0 ; i<m ; i++){
         Arrays.fill(dp[i] , -1);
        }
-       return findMin(m-1 , n-1 , grid , dp); 
-    }
-    public int findMin(int i , int j , int[][] grid , int [][]dp){
-        if(i==0 && j==0){
-            return grid[0][0];
+       dp[0][0] = grid[0][0];
+       for(int i=0 ; i<m ; i++){
+        for(int j=0 ; j<n ; j++){
+            int up = Integer.MAX_VALUE , left = Integer.MAX_VALUE;
+            if(i==0 && j==0){
+                continue;
+            }
+            if(i>0){
+                up = grid[i][j] + dp[i-1][j];
+            }
+            if(j>0){
+                left = grid[i][j] + dp[i][j-1];
+            }
+            dp[i][j] = Math.min(left, up);
         }
-        if(i<0 || j<0){
-            return 1000000000;
-        }
-        if(dp[i][j] != -1){
-            return dp[i][j];
-        }
-        int up = grid[i][j] + findMin(i-1 ,j , grid , dp);
-        int left = grid[i][j] + findMin(i , j-1 , grid , dp);
-
-        return dp[i][j] = Math.min(up , left);
-
+       }
+        return dp[m-1][n-1];
     }
 }
