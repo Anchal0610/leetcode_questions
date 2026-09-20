e74facef947118b5a1d50e423b010782c0fca10f
Anchal Gupta <anchalgupta0602@gmail.com>
2026-09-20 22:03:58 +0530

    Time: 1 ms (12.53%), Space: 43.1 MB (52.77%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.java
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.java
@@ -1,32 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
     public List<Integer> postorderTraversal(TreeNode root) {
-        Stack<TreeNode> st1 = new Stack<TreeNode>();
-        Stack<TreeNode> st2 = new Stack<TreeNode>();
-        List<Integer> ans = new ArrayList<>();
-
-        if(root == null){
-            return ans;
-        }
-
-        st1.push(root);
-
-        while(!st1.isEmpty()){
-            root = st1.pop();
-            st2.push(root);
-
-            if(root.left != null){
-                st1.push(root.left);
+        Stack<TreeNode> st = new Stack<TreeNode>();
+        ArrayList<Integer> ans = new ArrayList<>();
+        TreeNode curr = root;
+        while(curr != null || !st.isEmpty()){
+            if(curr != null){
+                st.push(curr);
+                curr = curr.left;
             }
-
-            if(root.right != null){
-                st1.push(root.right);
+            else{
+                TreeNode temp = st.peek().right;
+                if(temp ==  null){
+                    temp = st.peek();
+                    st.pop();
+                    ans.add(temp.val);
+                    while(!st.isEmpty() && temp == st.peek().right){
+                        temp = st.peek();
+                        st.pop();
+                        ans.add(temp.val);
+                    }
+                }
+                else{
+                        curr = temp;
+                    }
             }
         }
-
-        while(!st2.isEmpty()){
-            ans.add(st2.pop().val);
-        }
-
         return ans;
     }
 }
